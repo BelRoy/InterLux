1f7961c01b06448254454c29897cdea1f0336e7e
Vladislav Belous <oksiwhite@ukr.net>
2025-02-13 16:32:09 +0200

    pre finish

--- sender.php
+++ sender.php
@@ -1,13 +1,13 @@
-
-<?php
-if (isset($_POST['email']) && !empty($_POST['email'])) {
-$subject = "New contact request: $_POST['subject']";
-$message = $_POST['body'];
-$headers = 'From: support@interlux.com' . "\r\n" .
-           'Reply-To: ' . $_POST['email']. "\r\n" .
-           'X-Mailer: PHP/' . phpversion();
-
-mail('support@interlux.com', $subject, $message, $headers);
-
-die('Thank you for your email');
+
+<?php
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+$subject = "New contact request: $_POST['subject']";
+$message = $_POST['body'];
+$headers = 'From: support@interlux.com' . "\r\n" .
+           'Reply-To: ' . $_POST['email']. "\r\n" .
+           'X-Mailer: PHP/' . phpversion();
+
+mail('support@interlux.com', $subject, $message, $headers);
+
+die('Thank you for your email');
 }
